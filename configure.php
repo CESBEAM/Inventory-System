f6abf3d68773d17c984d3b64b99e6ed923d4440d
CESBEAM <softwareversion2.8@gmail.com>
2021-12-29 19:02:35 +0800

    added warning exist email, backend working on

--- configure.php
+++ configure.php
@@ -1,11 +1,11 @@
 <?php
 
 $server = "localhost";
-$canteenname = "root";
-$password = "";
-$database ="login_register_accounts";
+$canteen = "root";
+$pass = "";
+$database ="conpyx";
 
-$conn = mysqli_connect($server, $canteenname, $password, $database);
+$conn = mysqli_connect($server, $canteen, $pass, $database);
 
 if (!$conn){
     die("<script>alert('Connection Failed.')</script>");
--- register.php
+++ register.php
@@ -1,7 +1,9 @@
 <?php
    
  include 'configure.php';
-  
+ 
+  error_reporting(0);
+
  if(isset($_POST['submit'])){
     $canteenname = $_POST['canteenname'];
     $email = $_POST['email'];
@@ -9,11 +11,25 @@
     $conpassword = md5($_POST['conpassword']);
 
     if($password == $conpassword){
-        $sql =  "INSERT INTO users (canteenname, email, password)VALUES ('$canteenname', '$email', '$password')";
+        $sql = "SELECT * FROM users WHERE email = '$email'";
         $result = mysqli_query($conn, $sql);
-        if (!$result){
-          echo "<script>alert('Opps! Something wrong went.')</script>";
+        if (!$result -> num_rows > 0){
+          $sql =  "INSERT INTO users (canteenname, email, password) VALUES ('$canteenname', '$email', '$password')";
+          $result = mysqli_query($conn, $sql);
+          if ($result){
+            echo "<script>alert('Account Registration Success.')</script>";
+            $canteenname = "";
+            $email = "";
+            $_POST['password'] = "";
+            $_POST['conpassword'] = "";
+          }else{
+            echo "<script>alert('Opps! Something wrong went.')</script>";
         }
+          
+        } else {
+          echo "<script>alert('Email Already Exist.')</script>";
+        }
+        
     } else {
       echo "<script>alert('Password not matched.')</script>";
     }
@@ -37,22 +53,22 @@
 
         <div>
           <label for="canteenName">Canteen Name</label>
-          <input name ="canteenname" type="text" id="canteen-name" required>
+          <input name ="canteenname" type="text" id="canteen-name" value = "<?php echo $canteenname; ?>" required>
         </div>
 
         <div>
           <label for="email-reg">Email</label>
-          <input name = "email" type="email" id="email-reg" required>
+          <input name = "email" type="email" id="email-reg" value = "<?php echo $email; ?>" required>
         </div>
 
         <div>
           <label for="password">Password</label>
-          <input name = "password" type="password" id="password" required>
+          <input name = "password" type="password" id="password" value = "<?php echo $_POST['password']; ?>" required>
         </div>
 
         <div>
           <label for="con-password">Confirm Password</label>
-          <input type="password" id="con-password" required>
+          <input name ="conpassword" type="password" id="con-password" value = "<?php echo $_POST['conpassword']; ?>"  required>
         </div>
 
         <button name="submit" class="btn" value="SIGN UP">SIGN UP</button>
